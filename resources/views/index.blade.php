0a9649418ec9318889454f3f36540c2f70f2b3eb
Shvorobey <a.shvorobey@gmail.com>
2019-03-27 12:01:11 +0200

    Добавлена отладочная панель

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,10 +7,7 @@
         <small>Пожалуй, самый лучший в мире блог</small>
     </h1>
 
-        <!-- Blog Post -->
        @foreach( $posts as $post)
-
-
             <div class="card mb-4">
                    <img class="card-img-top" src="{{$post->img}}" alt="Card image cap">
                    <div class="card-body">
@@ -29,23 +26,15 @@
                    </div>
             </div>
         @endforeach
-
-
-    <!-- Pagination -->
-
-
             <ul class="pagination justify-content-center mb-4">
                 @if ($posts->currentPage()!=1)
                     <li><a class="page-item" href="?page=1"> Первая страница </a></li>
                     <li><a class="page-item" href="{{$posts->previousPageUrl()}}"> < </a></li>
                 @endif
-
                 @if ($posts->count ()>0)
                     @for ($count=1; $count<=$posts->lastPage(); $count++)
                         <li><a class="page-item @if ($count==$posts->currentPage()) current @endif" href="?page={{$count}}">{{$count}}</a></li>
-
                     @endfor
-
                 @else
                     <h1> <font size="15" color="aqua" face="Arial"> Мы работаем над тем, чтобы здесь что-то появилось ;) </font> </h1>
                 @endif
@@ -55,7 +44,6 @@
                 @endif
             </ul>
     </div>
-
 @endsection
 
 @section ('categories')
@@ -66,15 +54,10 @@
             <div class="row">
                 <div class="col-lg-6">
                     <ul class="list-unstyled mb-0">
-
                         @inject('categories', 'App\Categories_for_sidebar')
-
                         <div>
-
                              {{ $categories->show_categories() }}<br>
-
                         </div>
-
                     </ul>
                 </div>
             </div>
@@ -90,15 +73,10 @@
             <div class="row">
                 <div class="col-lg-6">
                     <ul class="list-unstyled mb-0">
-
                         @inject('autors', 'App\Autors_for_sidebar')
-
                         <div>
-
                              {{ $autors->show_autors() }}<br>
-
                         </div>
-
                     </ul>
                 </div>
             </div>
@@ -120,8 +98,6 @@
         </div>
     </div>
 @endsection
-
-
 
 @section ('advertising')
     <!-- Advertising Widget -->
